2d972accfb3e1c41f4946494bc50ae209b5a7cbc
dude719 <blood_099@hotmail.com>
2016-10-14 16:17:41 -0300

    Delete abstract.py

--- abstract.py
+++
@@ -1,6 +0,0 @@
-# --== Decompile ==--
-kOSRequestCodes = {'win32': 'W', 'linux': 'L', 'darwi': 'M', 'sunos': 'N', 'freeb': 'F', 'tru64': 'T', 'netbs': 'E', 'openb': 'B'}
-kVersionRequestCodes = {'2': 'X', '3': '3', '4': '4', '5': '5'}
-kRequestVersionCode = kVersionRequestCodes[config.kVersion[:config.kVersion.find('.')]]
-
-------
